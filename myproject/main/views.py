cfa6c2507436179ecb2ea56c22445169b615529e
MLGalusha <masonlgalusha@gmail.com>
2024-06-12 23:32:13 -0400

    learning Django

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from main.calculator import calculate
 
-# Create your views here.
 def home(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        return render(request, 'home.html', {'name': name})
-    return render(request, 'home.html')
+    result = None
+    greeting = None
+
+    if request.method == 'POST':
+        if 'name' in request.POST:
+            name = request.POST.get('name')
+            greeting = f"Hello, {name}."
+        elif 'num1' in request.POST and 'num2' in request.POST:
+            num1 = float(request.POST.get('num1'))
+            num2 = float(request.POST.get('num2'))
+            operation = request.POST.get('operation')
+            result = calculate(operation, num1, num2)
+
+    return render(request, 'main/home.html', {'result': result, 'greeting': greeting})
